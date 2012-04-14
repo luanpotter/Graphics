fba1187cd309a08ed5cd7c5f478b41a899625abe
Luan Nico <luanpotter28@hotmail.com>
2012-04-14 16:20:53 -0300

    Adição de algumas métodos interessantes, como distancia(Ponto p1, Ponto p2), entre outros.

--- ContextoGrafico.java
+++ ContextoGrafico.java
@@ -1,5 +1,7 @@
 package editorgrafico;
 
+import java.awt.Color;
+
 /**
  * @author Luan
  */
@@ -40,6 +42,22 @@ public class ContextoGrafico {
     
     public float desconverter(int medida) {
         return medida * Math.max((xSupDir - xInfEsq) / largura, (ySupDir - yInfEsq) / altura);
+    }
+    
+    public Ponto desconverterPonto (int x, int y) {
+        return new Ponto (desconverteX(x), desconverteY(y), Color.BLACK, 0);
+    }
+    
+    public float distanciaEntre (float x1, float y1, float x2, float y2) {
+        return (float) Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
+    }
+    
+    public float distanciaEntre (Ponto p1, Ponto p2) {
+        return distanciaEntre (p1.getX(), p1.getY(), p2.getX(), p2.getY());
+    }
+    
+    public float distancaiEntre (Ponto p1, int x2, int y2) {
+        return distanciaEntre (p1.getX(), p1.getY(), desconverteX(x2), desconverteY(y2));
     }
     
     @Override
